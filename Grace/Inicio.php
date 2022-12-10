14970cee4a3730b6bffbcfe03e09709acf50a5d6
jonathanOspina <117487375+jonathanOspina@users.noreply.github.com>
2022-12-09 20:21:58 -0500

    Add files via upload

---
+++ Grace/Inicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Grace</title>
+</head>
+<body>
+    <?php include_once "./Header.php" ?>
+    
+            <section class="inicioo" id="inicioo">
+                <div class="contentt">
+                    <h3>Historia</h3>
+                    <p>Grace Murray Hopper nació en 1906. Fue una científica y militar estadounidense, y una de las primeras programadoras de la computadora Harvard Mark I. Durante las décadas del 50 y 60, inventó el primer compilador de un lenguaje de programación de computadora y también estuvo a cargo de popularizar la idea de lenguajes de programación independientes de la máquina. <br> Amazing Grace, como la llamaban su amistades, fue una mujer de grandes ideas. Tal es así que influyó en el desarrollo de COBOL, uno de los primeros lenguajes de programación de alto nivel.</p>
+                </div>
+                <div class="imagee">
+                    <img src="img/grace.jpeg" alt="">
+                </div>
+            </section>
+</body>
+</html>
